4772359d610c3ab8f85117e569d8d3d4797c2979
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-02-22 12:45:57 +0000

    Add components to the dashboard page

--- pages/dashboard/__tests__/DashboardIndexPage.spec.js
+++ pages/dashboard/__tests__/DashboardIndexPage.spec.js
@@ -1,8 +1,57 @@
-import { mount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import DashboardIndexPage from '~/pages/dashboard/index.vue'
+import FileUploader from '~/components/FileUploader.vue'
+import * as rootStore from '~/store'
+import DownloadJson from '~/components/DownloadJson.vue'
+import DataTable from '~/components/DataTable.vue'
+import PaginationControls from '~/components/PaginationControls.vue'
 
 describe('DashboardIndexPage', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+
   it('should mount', () => {
-    expect(() => mount(DashboardIndexPage)).not.toThrow()
+    expect(() =>
+      mount(DashboardIndexPage, {
+        localVue,
+        store: new Vuex.Store({ ...rootStore })
+      })
+    ).not.toThrow()
+  })
+
+  describe('when mounted', () => {
+    let store, wrapper
+    beforeEach(() => {
+      store = new Vuex.Store({ ...rootStore })
+      wrapper = mount(DashboardIndexPage, {
+        localVue,
+        store
+      })
+    })
+
+    it('should render a FileUploader component', () => {
+      expect(wrapper.find(FileUploader).exists()).toBeTruthy()
+    })
+
+    it('should render a DownloadJson component', () => {
+      expect(wrapper.find(DownloadJson).exists()).toBeTruthy()
+    })
+
+    it('should render a DownloadJson component', () => {
+      expect(wrapper.find(DownloadJson).exists()).toBeTruthy()
+    })
+
+    it('should render a DataTable component', () => {
+      expect(wrapper.find(DataTable).exists()).toBeTruthy()
+    })
+
+    it('should render a DataTable component', () => {
+      expect(wrapper.find(DataTable).exists()).toBeTruthy()
+    })
+
+    it('should render a PaginationControls component', () => {
+      expect(wrapper.find(PaginationControls).exists()).toBeTruthy()
+    })
   })
 })
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>
-    hello this is dashboard
+  <div class="w-full">
+    <file-uploader
+      :accept="['text/csv']"
+      class="mb-2"
+      @upload="$store.dispatch('parseFile', $event)"
+    />
+
+    <div>
+      <download-json />
+
+      <div class="border border-gray-700">
+        <data-table class="w-full h-64" :labels="labels" :rows="rows" />
+
+        <pagination-controls
+          class="border-t border-gray-700"
+          :current-page="currentPage"
+          :page-count="pageCount"
+          @pageChange="$store.dispatch('setPage', $event)"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+import FileUploader from '~/components/FileUploader.vue'
+import DataTable from '~/components/DataTable.vue'
+import DownloadJson from '~/components/DownloadJson.vue'
+import PaginationControls from '~/components/PaginationControls.vue'
+
 export default {
-  layout: 'page'
+  layout: 'page',
+
+  components: {
+    FileUploader,
+    DataTable,
+    DownloadJson,
+    PaginationControls
+  },
+
+  computed: {
+    ...mapState(['currentPage', 'labels']),
+    ...mapGetters(['pageCount', 'getRows']),
+
+    rows() {
+      return this.getRows(this.currentPage)
+    }
+  }
 }
 </script>
