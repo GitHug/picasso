dd8e20d06fae61509aaa7cfbe7217e7b821ab71d
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-02-22 12:48:05 +0000

    Rename describe statement

--- store/__tests__/index.spec.js
+++ store/__tests__/index.spec.js
@@ -130,7 +130,7 @@ describe('Store', () => {
     })
   })
 
-  describe('pageCount action', () => {
+  describe('pageCount getter', () => {
     const rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     const perPage = 3
 
