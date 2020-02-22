60d1773d08481105204bfdb2bc5c71794aca427c
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-02-22 12:47:21 +0000

    Add add navbar to layout

--- layouts/page.vue
+++ layouts/page.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <nuxt />
+  <div id="main" class="flex overflow-hidden">
+    <div class="fixed w-screen h-8 bg-green-400">
+      <nuxt-link
+        :active-class="`border-b border-gray-700 font-semibold`"
+        to="/dashboard"
+      >
+        Dashboard
+      </nuxt-link>
+    </div>
+
+    <div id="content" class="overflow-y-auto w-screen mt-8 px-6 pt-4">
+      <nuxt />
+    </div>
   </div>
 </template>
