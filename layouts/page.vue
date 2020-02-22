64d3a03dfc05ce1acad1421f504440b9a36cec18
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-02-22 21:42:32 +0000

    Add users page to navbar

--- layouts/page.vue
+++ layouts/page.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="main" class="flex overflow-hidden">
-    <div class="fixed w-screen h-8 bg-green-400">
+    <nav class="fixed w-screen h-8 bg-green-400">
       <nuxt-link
         :active-class="`border-b border-gray-700 font-semibold`"
         to="/dashboard"
       >
         Dashboard
       </nuxt-link>
-    </div>
+
+      <nuxt-link
+        :active-class="`border-b border-gray-700 font-semibold`"
+        to="/users"
+      >
+        users
+      </nuxt-link>
+    </nav>
 
     <div id="content" class="overflow-y-auto w-screen mt-8 px-6 pt-4">
       <nuxt />
