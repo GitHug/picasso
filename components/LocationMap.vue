e9ed998e106e6bda8fff64144866920bc095c4d4
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-02-24 21:48:35 +0000

    Change tile layer url to https

--- components/LocationMap.vue
+++ components/LocationMap.vue
@@ -2,7 +2,7 @@
   <div id="map-wrap">
     <no-ssr>
       <l-map :zoom="4" :center="[latitude, longitude]">
-        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" />
         <l-marker :lat-lng="[latitude, longitude]" />
       </l-map>
     </no-ssr>
