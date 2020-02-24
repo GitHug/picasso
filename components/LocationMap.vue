a0b5b4b8f4b07f064793eda2e789f1fc1bc10dd8
Fredrik Mäkilä <fredrik.makila@gmail.com>
2020-02-24 22:09:30 +0000

    Add Openstreetmap contribution

--- components/LocationMap.vue
+++ components/LocationMap.vue
@@ -2,7 +2,10 @@
   <div id="map-wrap">
     <client-only>
       <l-map :zoom="4" :center="[latitude, longitude]">
-        <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          :attribution="attribution"
+        />
         <l-marker :lat-lng="[latitude, longitude]" />
       </l-map>
     </client-only>
@@ -19,6 +22,12 @@ export default {
     longitude: {
       type: Number,
       required: true
+    }
+  },
+  data() {
+    return {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }
   }
 }
